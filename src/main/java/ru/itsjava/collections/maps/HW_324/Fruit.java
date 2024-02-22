54357d49323260dd9d174ce09059a94e57a08a02
YakovBSergeev <154790912+YakovBSergeev@users.noreply.github.com>
2024-02-22 11:17:33 +0600

    HW-324

---
+++ src/main/java/ru/itsjava/collections/maps/HW_324/Fruit.java
@@ -0,0 +1,12 @@
+package ru.itsjava.collections.maps.HW_324;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+
+public class Fruit {
+    private String name;
+    private double weight;
+}
---
+++ src/main/java/ru/itsjava/collections/maps/HW_324/Man.java
@@ -0,0 +1,12 @@
+package ru.itsjava.collections.maps.HW_324;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class Man implements Serializable {
+    private final String name;
+}
